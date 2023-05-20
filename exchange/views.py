b46a04ee480357659ddf7a9dcce30791f8c1d3f1
MoulineRo <simonovandsim@gmail.com>
2023-05-21 02:33:46 +0300

    for bootstrap

--- exchange/views.py
+++ exchange/views.py
@@ -9,7 +9,6 @@ from django.shortcuts import render
 from exchange.forms import CounterForm
 from .models import Counter
 from .models import Rate
-from .tasks import start_exchange
 
 
 class DecimalAsFloatJSONEncoder(DjangoJSONEncoder):
@@ -19,21 +18,8 @@ class DecimalAsFloatJSONEncoder(DjangoJSONEncoder):
         return super().default(o)
 
 
-# Create your views here.
-
-
 def index(request):
     if request.method == "GET":
-        start_exchange.delay("mono", "USD", "UAH")
-        start_exchange.delay("privat", "USD", "UAH")
-        start_exchange.delay("vkurse", "USD", "UAH")
-        start_exchange.delay("nbu", "USD", "UAH")
-        start_exchange.delay("minfin", "USD", "UAH")
-        start_exchange.delay("mono", "EUR", "UAH")
-        start_exchange.delay("privat", "EUR", "UAH")
-        start_exchange.delay("vkurse", "EUR", "UAH")
-        start_exchange.delay("nbu", "EUR", "UAH")
-        start_exchange.delay("minfin", "EUR", "UAH")
         current_date = datetime.date.today()
         current_ratesUs = Rate.objects.filter(date=current_date, currency_a='USD').all().values().aggregate(Min('sell'))
         current_ratesUb = Rate.objects.filter(date=current_date, currency_a='USD').all().values().aggregate(Min('buy'))
@@ -45,10 +31,10 @@ def index(request):
         currES = float(current_ratesEs['sell__min'])
         currEB = float(current_ratesEb['buy__min'])
         form = CounterForm()
-        context = {"form": form, 'message1': str(currUS) + ' Put this full name of currency> USD-sell ',
-                   'message2': str(currUB) + ' Put this full name of currency> USD-buy ',
-                   'message3': str(currES) + ' Put this full name of currency> EUR-sell ',
-                   'message4': str(currEB) + ' Put this full name of currency> EUR-buy '}
+        context = {"form": form, 'message1': str(currUS) + ' USD-sell ',
+                   'message2': str(currUB) + ' USD-buy ',
+                   'message3': str(currES) + ' EUR-sell ',
+                   'message4': str(currEB) + ' EUR-buy '}
         return render(request, "counter.html", context)
     elif request.method == "POST":
         form = CounterForm(request.POST)
@@ -72,13 +58,13 @@ def index(request):
             currEB = float(current_ratesEb['buy__min'])
             if which == "USD-sell":
                 res1 = currUS * count
-                return HttpResponse(str(res1) + ' USD')
+                return HttpResponse(str(res1) + ' UAH')
             if which == "USD-buy":
                 res2 = currUB * count
-                return HttpResponse(str(res2) + ' USD')
+                return HttpResponse(str(res2) + ' UAH')
             if which == "EUR-sell":
                 res2 = currES * count
-                return HttpResponse(str(res2) + ' EUR')
+                return HttpResponse(str(res2) + ' UAH')
             if which == "EUR-buy":
                 res2 = currEB * count
-                return HttpResponse(str(res2) + ' EUR')
+                return HttpResponse(str(res2) + ' UAH')
